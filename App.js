f52ab57e5b7f2583cacc01d155c4dd8c676a04ec
Zach Capshaw <zcapshaw@gmail.com>
2018-11-24 18:01:12 -0700

    Add library component and reducer

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import { Header } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View />
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
       </Provider>
     );
   }
