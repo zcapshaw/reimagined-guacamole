d078975f8ed18231cc2cf3cfd53e0d5161a69ddd
Zach Capshaw <zcapshaw@gmail.com>
2018-11-24 18:58:39 -0700

    Add ListItem

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
+        <View style={{ flex: 1 }}>
           <Header headerText="Tech Stack" />
+          <LibraryList />
         </View>
       </Provider>
     );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
-export default class LibraryList extends Component {
+class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id.toString()}
+      />
+    );
   }
 }
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
